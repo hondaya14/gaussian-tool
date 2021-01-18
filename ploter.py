66159d3db0d75cb5a510c185d2d5bed514066c2b
Yasuhisa Honda <nqvno14@gmail.com>
2021-01-18 16:39:22 +0900

    ボクセル のセクションをヒートマップで保存するプログラムを追加

--- ploter.py
+++ ploter.py
@@ -1,35 +1,82 @@
+import sys
+import seaborn as sb
 import numpy as np
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-X, Y, Z, V = [], [], [], []
-while True:
-    try:
-        x, y, z, value = list(map(float, input().split()))
-        X.append(x)
-        Y.append(y)
-        Z.append(z)
-        V.append(value)
-    except EOFError:
-        break
+# voxel params
+x_number = 130
+y_number = 100
+z_number = 76
 
-X = np.array(X)
-Y = np.array(Y)
-Z = np.array(Z)
-V = np.array(V)
 
-plt.rcParams['font.size'] = 14
-plt.rcParams['font.family'] = 'Times New Roman'
+# なに軸の何番目か
+section_axis = sys.argv[1]  # x, y, z
+# section_number = sys.argv[2]  # 何枚目
+print('section axis: ' + section_axis)
+# print('section number: ' + section_number)
 
-fig = plt.figure()
-ax1 = Axes3D(fig)
+section_number = 0
+if section_axis == 'x':
+    section_number = x_number
+elif section_axis == 'y':
+    section_number = y_number
+elif section_axis == 'z':
+    section_number = z_number
 
-ax1.set_xlabel('x')
-ax1.set_ylabel('y')
-ax1.set_zlabel('z')
 
-ax1.scatter(X, Y, V)
-plt.legend()
+def validate_axis(xi, yi, zi, sec_num):
+    if section_axis == 'x':
+        if xi == sec_num:
+            return True
+    elif section_axis == 'y':
+        if yi == sec_num:
+            return True
+    elif section_axis == 'z':
+        if zi == sec_num:
+            return True
+    return False
 
-plt.show()
-plt.close()
+
+# prepare data
+data_max = 0
+data_min = 100000000
+voxel = [[[0] * z_number for i in range(y_number)] for j in range(x_number)]
+for k in range(z_number):
+    for j in range(y_number):
+        for i in range(x_number):
+            x, y, z, value = map(float, input().split())
+            voxel[i][j][k] = value
+            data_max = max(data_max, value)
+            data_min = min(data_min, value)
+
+section = np.array([[]])
+
+if section_axis == 'x':
+    section = [[0] * z_number for i in range(y_number)]
+elif section_axis == 'y':
+    section = np.array([[0] * z_number for i in range(x_number)])
+elif section_axis == 'z':
+    section = np.array([[0] * y_number for i in range(x_number)])
+
+
+def make_plane(xi, yi, zi):
+    if section_axis == 'x':
+        section[yi][zi] = voxel[xi][yi][zi]
+    elif section_axis == 'y':
+        section[xi][zi] = voxel[xi][yi][zi]
+    elif section_axis == 'z':
+        section[xi][yi] = voxel[xi][yi][zi]
+
+
+save_path = 'test/test_section/'
+for sn in range(section_number):
+    for i in range(x_number):
+        for j in range(y_number):
+            for k in range(z_number):
+                if validate_axis(i, j, k, sn):
+                    make_plane(i, j, k)
+    plt.figure()
+    sb.heatmap(section, cmap='Blues', vmax=data_max, vmin=data_min)
+    # plt.show()
+    plt.savefig(save_path+section_axis+str(sn)+'.png')
+
